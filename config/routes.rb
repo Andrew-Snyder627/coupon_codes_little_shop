c6da4270a64adbc12ee19e52e44bb378041cc146
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-04-25 15:57:11 -0400

    Refactors the routes files to function with resources syntax

--- config/routes.rb
+++ config/routes.rb
@@ -4,42 +4,35 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  
-
-
-
-
   # Defines the root path route ("/")
   # root "posts#index"
-  #non RESTful endpoints
-  get "/api/v1/merchants/find", to: "api/v1/merchants#find"
-  # Merchant Endpoints
-  get "/api/v1/merchants", to: "api/v1/merchants#index"
-  get "/api/v1/items", to: "api/v1/items#index"
-  get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
-  post "/api/v1/merchants", to: "api/v1/merchants#create"
-  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
-  delete "/api/v1/merchants/:id", to: "api/v1/merchants#destroy"
- 
-  # Item Endpoints
 
-  # Non-RESTful Find endpoints for Items
-  get "/api/v1/items/find", to: "api/v1/items_search#find"
-  get "/api/v1/items/find_all", to: "api/v1/items_search#find_all"
-  
-  get "/api/v1/items", to: "api/v1/items#index"
-  get "/api/v1/items/:id", to: "api/v1/items#show"
-  post "/api/v1/items", to: "api/v1/items#create"
-  patch "/api/v1/items/:id", to: "api/v1/items#update"
-  put "/api/v1/items/:id", to: "api/v1/items#update"
-  delete "/api/v1/items/:id", to: "api/v1/items#destroy"
-  
-  #relationship endpoints
-  get "/api/v1/merchants/:id/items", to: "api/v1/merchant_items#index"
-  get "/api/v1/items/:id/merchant", to: "api/v1/item_merchants#show"
-  get "/api/v1/merchants/:merchant_id/invoices", to: "api/v1/invoices#index"
-  get "/api/v1/merchants/:merchant_id/customers", to: "api/v1/merchants_customers#index"
+  namespace :api do
+    namespace :v1 do
 
- 
+      # Merchants
+      resources :merchants do
+        resources :customers, only: [:index], controller: "merchants_customers"
+        resources :invoices, only: [:index]
+        resources :items, only: [:index]
+        resources :items, only: [:index], controller: "merchant_items", as: "merchant_items"
+      end
+
+      # Items
+      resources :items
+      get "items/find", to: "items_search#find"
+      get "items/find_all", to: "items_search#find_all"
+      get "items/:id/merchant", to: "item_merchants#show"
+
+      # Coupons
+      namespace :merchant_coupons do
+        resources :merchants, only: [] do
+          resources :coupons, only: [:index, :show, :create, :update]
+        end
+      end
+
+      # Non-RESTful merchant search
+      get "merchants/find", to: "merchants#find"
+    end
+  end
 end
