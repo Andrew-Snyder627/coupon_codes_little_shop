49f396f9327b29fc0e132852cde73619a64c9a9b
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-04-25 12:55:42 -0400

    Adds in index, show, & create logic, next step adding class methods to coupon model

--- app/controllers/api/v1/merchant_coupons/coupons_controller.rb
+++ app/controllers/api/v1/merchant_coupons/coupons_controller.rb
@@ -1,14 +1,25 @@
 class Api::V1::MerchantCoupons::CouponsController < ApplicationController
   def index
-
+    merchant = Merchant.find(params[:merchant_id])
+    coupons = merchant.coupons
+    render json: CouponSerializer.new(coupons)
   end
 
   def show
-
+    coupon = Merchant.find(params[:merchant_id]).coupons.find(params[:id])
+    render json: CouponSerializer.new(coupon)
   end
 
   def create
+    merchant = Merchant.find(params[:merchant_id])
+    coupons = merchant.coupons.new(coupon_params)
 
+    if Coupon.exceeds_active_limit?(merchant) && coupon.active?
+      raise ActiveRecord::RecordInvalid.new(coupon), "This Merchant already has 5 active coupons"
+    end
+
+    coupon.save!
+    render json: CouponSerializer.new(coupon), status: :created
   end
 
   def update
@@ -16,5 +27,5 @@ class Api::V1::MerchantCoupons::CouponsController < ApplicationController
   end
 
   private
-    
+
 end
