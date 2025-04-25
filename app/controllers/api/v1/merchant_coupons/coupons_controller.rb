ad659014b932710b6852ffcbd89709e05f889759
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-04-25 15:30:44 -0400

    Adds in model and controller logic, next refactoring routes in resource notation and adding in new routes for testing

--- app/controllers/api/v1/merchant_coupons/coupons_controller.rb
+++ app/controllers/api/v1/merchant_coupons/coupons_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::MerchantCoupons::CouponsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :incomplete_response
+  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :malformed_json_response
+  
   def index
     merchant = Merchant.find(params[:merchant_id])
     coupons = merchant.coupons
@@ -23,9 +27,35 @@ class Api::V1::MerchantCoupons::CouponsController < ApplicationController
   end
 
   def update
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = merchant.coupons.find(params[:id])
 
+    if Coupon.prevent_deactivation?(coupon, coupon_params)
+      raise ActiveRecord::RecordInvalid.new(coupon), "Coupon cannot be deactivated due to pending invoices"
+    end
+
+    if Coupon.exceeds_active_limit?(merchant, coupon_params)
+      raise ActiveRecord::RecordInvalid.new(coupon), "This Merchant already has 5 active coupons"
+    end
+
+    coupon.update!(coupon_params)
+    render json: CouponSerializer.new(coupon)
   end
 
   private
+    def coupon_params
+      params.require(:coupon).permit(:name, :code, :value, :value_type, :active)
+    end
 
+    def not_found_response(exception)
+      render json: ErrorSerializer.serialize(exception), status: :not_found
+    end
+
+    def incomplete_response(exception)
+      render json: ErrorSerializer.serialize(exception), status: :bad_request
+    end
+
+    def malformed_json_response(exception)
+      render json: ErrorSerializer.serialize(exception), status: :bad_request
+    end
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -8,6 +8,16 @@ class Coupon < ApplicationRecord
   validates :value_type, inclusion: { in: %w[dollar percent] }
   validates :active, inclusion: { in: [true, false] }
 
+  def self.exceeds_active_limit?(merchant, params = {})
+  activating = params[:active] == true || params[:active] == "true"
+  activating && merchant.coupons.where(active: true).count >= 5
+  end
+
+  def self.prevent_deactivation?(coupon, params = {})
+  deactivating = params[:active] == false || params[:active] == "false"
+  deactivating && coupon.invoices.where(status: "pending").any?
+  end
+
   def times_used
     invoices.count 
   end
