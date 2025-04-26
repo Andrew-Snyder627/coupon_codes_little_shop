c0a00cfbe20f5caaa324713620c1b004453a4e7e
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-04-25 21:11:11 -0400

    Adds in shoulda matcher testing for all relationships, tests pass

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many :invoices }
+  end
+
+  describe "validations" do
+    subject { create(:coupon) } # Shouldamatchers must have something to validate against with uniqueness
+
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :code }
+    it { should validate_uniqueness_of :code}
+    it { should validate_presence_of :value }
+    it { should validate_inclusion_of(:value_type).in_array(%w[dollar percent]) }
+    it { should allow_value(true).for(:active) }
+    it { should allow_value(false).for(:active) }
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,6 +4,7 @@ require "spec_helper"
 RSpec.describe Invoice, type: :model do
   it { should belong_to :customer }
   it { should belong_to :merchant }
+  it { should belong_to(:coupon).optional }
   it { should have_many :invoice_items }
   it { should have_many :transactions }
 
